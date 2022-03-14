07548403560adc1a4a33fc48f7a9ce76aae6ef2e
Adrien Lefevre <adrien.lefevre@edu.devinci.fr>
2022-03-14 17:23:45 +0100

    maj slr

--- core/hal/drivers/slr/slr.py
+++ core/hal/drivers/slr/slr.py
@@ -3,9 +3,10 @@
 import os
 import sys
 import time
-
+import numpy as np
 import core.hal.drivers.slr.utils.get_sign as gs
 from core.hal.drivers.driver import BaseDriver
+
 
 class Driver(BaseDriver):
     """Sign Language Recognition driver"""
@@ -14,30 +15,83 @@ class Driver(BaseDriver):
         super().__init__(name, parent)
 
         self.register_to_driver("pose", "raw_data")
+        #self.register_to_driver("pose", "not_croped_raw_data")
         self.create_event("new_sign")
 
-        self.model = gs.init()
+        self.SLR_ACTIONS = [
+            "nothing",
+            "empty",
+            "hello",
+            "thanks",
+            "iloveyou",
+            "what's up",
+            "hey",
+            "my",
+            "name",
+            "nice",
+            "to meet you",
+        ]
+
+        self.model = gs.init(len(self.SLR_ACTIONS))
         self.frames = []
-        self.fps = 60
+        self.fps = 20
+        self.timeloop_start = 0
+        self.update_time = 0
+        self.count_update = 0
 
     def loop(self):
         start_t = time.time()
 
+        #frame = bytes_to_dict(self.parent.get_driver_event_data("pose", "raw_data"))
+
+        # "face_mesh": faces_landmarks,
+        # "body_pose": body_landmarks,
+        # "right_hand_pose": left_hands_landmarks
+        # "left_hand_pose": right_hands_landmarks
+
         frame = self.parent.get_driver_event_data("pose", "raw_data")
-
+        
         if frame is not None:
+            if self.frames:
+                if list(frame) != list(self.frames)[-1]:
 
-            if len(self.frames) < 30:
-                self.frames.append(frame)
+                    if len(self.frames) < 30:
+                        self.frames.append(gs.adapt_data(frame))
+                        self.count_update +=1
 
-            elif len(self.frames) == 30:
-                self.frames.pop(0)
-                self.frames.append(frame)
-                sign, probability = gs.get_sign(self.model, self.frames)
-                self.set_event_data(
-                    "new_sign", {"sign": sign, "probability": probability}
-                )
-                #(sign," ", probability)
+                    elif len(self.frames) == 30:
+                        #print(np.array(frame).shape)
+
+                        self.frames.append(gs.adapt_data(frame))
+                        self.count_update +=1
+
+
+                        timeloop = time.time()-self.timeloop_start
+                        while(timeloop < 0.09):
+                            timeloop = time.time()-self.timeloop_start
+                        self.timeloop_start = time.time()
+                        #print("TIMELOOP", timeloop)
+
+                        self.frames = self.frames[-30:]
+                        guessed_sign, probability = gs.get_sign(
+                                self.model, self.frames, self.SLR_ACTIONS)
+                        if(self.count_update >= 60):
+                            self.count_update = 0
+                            
+
+                            #print("waiting : ", time.time() - self.update_time)
+                            #print("proba * 2000 ", probability*2000)
+                            #if(time.time() - self.update_time > probability*2000):
+
+                            guessed_sign, probability = gs.get_sign(
+                                self.model, self.frames, self.SLR_ACTIONS)
+                        self.set_event_data(
+                            "new_sign", {"guessed_sign": guessed_sign, "probability": probability, "actions": self.SLR_ACTIONS}
+                        )
+                            #self.update_time = time.time()
+                        
+            else:
+                self.frames.append(gs.adapt_data(frame))
 
         end_t = time.time()
 
--- core/hal/drivers/slr/utils/get_sign.py
+++ core/hal/drivers/slr/utils/get_sign.py
@@ -3,66 +3,45 @@ import os
 import numpy as np
 # pip install onnx
 import onnxruntime
+import onnxruntime as ort
+import time
 
-SLR_ACTIONS = [
-    "nothing",
-    "empty",
-    "hello",
-    "thanks",
-    "iloveyou",
-    "what's up",
-    "hey",
-    "my",
-    "name",
-    "nice",
-    "to meet you",
-]
-
-
-def adapt_data(sequences: list) -> list:
+def adapt_data(frame: list) -> list:
     """
     Adapt data to the model
+    image shape[0] = 480
+    frape.shape[1] = 640
     """
+    
 
-    new_sequences = []
-    for _, sequence in enumerate(sequences):
-        pose = np.array([[res[0]/640, res[1]/480] for res in sequence["body_pose"]]).flatten(
-        ) if sequence["body_pose"] else np.zeros(33*2)
-        # face = np.array([[res[0], res[1], res.z] for res in sequence.face_landmarks.landmark]).flatten(
-        # ) if sequence.face_landmarks else np.zeros(468*3)
-        lh = np.array([[res[0]/640, res[1]/480] for res in sequence["left_hand_pose"]]).flatten(
-        ) if sequence["left_hand_pose"] else np.zeros(21*2)
-        rh = np.array([[res[0]/640, res[1]/480] for res in sequence["right_hand_pose"]]).flatten(
-        ) if sequence["right_hand_pose"] else np.zeros(21*2)
-        new_sequences.append(np.concatenate([pose, lh, rh]))
+    pose = np.array([[res[0], res[1]] for res in frame["body_pose"]]).flatten(
+        ) if frame["body_pose"] else np.zeros(33*2)
+    lh = np.array([[res[0], res[1]] for res in frame["left_hand_pose"]]).flatten(
+    ) if frame["left_hand_pose"] else np.zeros(21*2)
+    rh = np.array([[res[0], res[1]] for res in frame["right_hand_pose"]]).flatten(
+    ) if frame["right_hand_pose"] else np.zeros(21*2)  
+    return np.concatenate([pose, lh, rh])
 
-    return new_sequences
-
-
-def init():
+def init(output_size):
     """
     Initialize the module
     """
-    input_size = 150
     dir_path = os.path.dirname(os.path.realpath(__file__))
     #dummy_input = torch.randn(30, input_size, requires_grad=True)
 
-    model_path = os.path.join(dir_path, "../models/slr.onnx")
-    model = onnxruntime.InferenceSession(model_path)
+    model_path = os.path.join(dir_path, "../models/slr_"+str(output_size)+".onnx")
+    model = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'])
     return model
-    # model = onnx.load_state_dict(state_dict)
-    # torch.onnx.export(model, dummy_input, "action.onnx")
-    # onnx.save(model, dir_path + "models/")
-    # #model = onnxruntime.InferenceSession(model_path)
-    # return model
 
 
-def get_sign(model, frames: list) -> list:
+def get_sign(model, sequence, actions) -> list:
     """
     Get sign from frames
     """
-    data = adapt_data(frames)
-    ort_inputs = {model.get_inputs()[0].name: np.array([data], dtype=np.float32)}
+    #print("input shape: ", np.array([sequence], dtype=np.float32).shape)
+    #ort_inputs = {'input': np.array(
+    ort_inputs = {'input': np.array(
+    [sequence], dtype=np.float32)}
     out = model.run(None, ort_inputs)[-1]
     out = np.exp(out) / np.sum(np.exp(out))
-    return (SLR_ACTIONS[np.argmax(out)], float(np.max(out)))
+    return (actions[np.argmax(out)], float(np.max(out)))
