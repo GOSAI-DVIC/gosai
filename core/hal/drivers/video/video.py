bf3de525940a482cd539613f1fc8b0c14ce939c8
Thomas <thomas.juldo@edu.devinci.fr>
2022-03-17 21:46:34 +0100

    Better config verification

--- core/hal/drivers/video/video.py
+++ core/hal/drivers/video/video.py
@@ -28,27 +28,29 @@ class Driver(BaseDriver):
         self.create_event("depth")
         self.create_event("source")
 
-
     def pre_run(self):
         # self.source = IntelCamera(640, 480)
         if path.exists("home/config.json"):
-            with open("home/config.json","r") as f:
+            with open("home/config.json", "r") as f:
                 config = json.load(f)
-                if "camera" in config and "type" in config["camera"]:
+                if (
+                    "camera" in config
+                    and "type" in config["camera"]
+                    and "width" in config["camera"]
+                    and "height" in config["camera"]
+                ):
                     if config["camera"]["type"] == "standard":
                         self.source = StandardCamera(
-                            config["camera"]["width"],
-                            config["camera"]["height"]
+                            config["camera"]["width"], config["camera"]["height"]
                         )
                     elif config["camera"]["type"] == "intel":
                         self.source = IntelCamera(
-                            config["camera"]["width"],
-                            config["camera"]["height"])
+                            config["camera"]["width"], config["camera"]["height"]
+                        )
                 else:
                     self.source = StandardCamera(1920, 1080)
         else:
             self.source = StandardCamera(1920, 1080)
-
 
     def loop(self):
         # print(self.source)
@@ -60,7 +62,6 @@ class Driver(BaseDriver):
             self.set_event_data("depth", depth)
 
         time.sleep(1 / self.fps)  # Runs faster to be sure to get the current frame
-
 
     def execute(self, command, arguments=""):
         super().execute(command, arguments)
