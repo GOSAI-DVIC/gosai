cf4872c7040c9471730ba44708cd28ec5aca7e15
Thomas <thomas.juldo@edu.devinci.fr>
2022-03-22 15:11:10 +0100

    Mono server + Flask routes

--- core/server/server.py
+++ core/server/server.py
@@ -1,34 +1,17 @@
-import logging
 import os
-import sys
-import time
+import threading
 from queue import Queue
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import threading
-
-# import eventlet
-# import socketio
-
-# logging.getLogger("socketio").setLevel(logging.ERROR)
-# logging.getLogger("engineio").setLevel(logging.ERROR)
-# logging.getLogger("eventlet").setLevel(logging.ERROR)
-
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO
-
-
-class quietServer(SimpleHTTPRequestHandler):
-    def log_message(self, format, *args):
-        pass
 
 
 def start_chrome():
     def _start_chrome():
         options = ["--start-fullscreen", "--incognito", "--disable-logging"]
         options_line = " ".join(options)
-        cmd = f"chromium http://127.0.0.1:8000/core/display {options_line}"
+        cmd = f"chromium http://127.0.0.1:5000/platform {options_line}"
         os.system(f'/bin/bash -c "{cmd} &> /dev/null"')
 
     threading.Thread(target=_start_chrome, args=(), daemon=True).start()
@@ -36,14 +19,10 @@ def start_chrome():
 
 class Server:
     def __init__(self, hal):
-        # self.sio = socketio.Server(
-        #     logger=True,
-        #     cors_allowed_origins="*"
-        # )  # Creates the socketio server
-
-        # self.app = socketio.WSGIApp(self.sio)
-
         self.app = Flask(__name__)
+        self.app.config["SECRET_KEY"] = "secret!"
+        CORS(self.app)
+
         self.sio = SocketIO(
             self.app,
             logger=False,
@@ -55,30 +34,34 @@ class Server:
         self.hal = hal
 
         self.queue = Queue()
-        # self.started = False
 
         @self.sio.on("connect")
         def connect(*args):
-            # self.hal.log("server: Client connected")
             self.sio.start_background_task(self.send_queued_data)
 
-    def start(self, socket_port=5000, server_port=8000):
+        @self.app.route("/")
+        def home():
+            return "OK"
+
+        @self.app.route("/platform")
+        def platform():
+            return render_template("display/index.html")
+
+        @self.app.route("/platform/<path:path>")
+        def platform_path(path):
+            return send_from_directory("templates/display", path)
+
+        @self.app.route("/platform/home/<path:path>")
+        def platform_home_path(path):
+            return send_from_directory("../../home", path)
+
+
+    def start(self, socket_port=5000):
         """Starts the server on the given port"""
 
         def _start_socket(self, port):
-
-            # eventlet.wsgi.server(eventlet.listen(("", port)), self.app, log_output=True)
-
             self.sio.run(self.app, host="0.0.0.0", port=port)
 
-        def _start_server(self, port):
-            server_address = ("0.0.0.0", port)
-            self.httpd = HTTPServer(server_address, quietServer)
-            self.httpd.serve_forever()
-
-        threading.Thread(
-            target=_start_server, args=(self, server_port), daemon=True
-        ).start()
         threading.Thread(
             target=_start_socket, args=(self, socket_port), daemon=True
         ).start()
@@ -104,5 +87,3 @@ class Server:
             if not self.queue.empty():
                 name, data = self.queue.get()
                 self.sio.emit(name, data)
-            # if self.data != ():
-            #     name, data = self.data
