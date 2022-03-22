1b13af2738ef03744d1d575f8d0ce054b2789d3b
Maxime <mbroussart@gmail.com>
2022-03-22 15:26:30 +0100

    Cue driver development

--- core/hal/drivers/cue/cue.py
+++ core/hal/drivers/cue/cue.py
@@ -1,37 +1,176 @@
-# cues estimation Driver
-# Return a list containing x,y cue positions
+# Cue estimation Driver
+# Return a list: [cue, x, y]
+# cue: boolean    => True/False indicating if there is a cue or not
+# a  : (int, int) => couple 1 of extreme cue coordinates
+# b  : (int, int) => couple 2 of extreme cue coordinates
 
+from __future__ import annotations
+from core.hal.drivers.driver import BaseDriver
+from dataclasses import dataclass
+from typing import Any
+
+import cv2
+import json
+import numpy as np
 import time
 
-import core.hal.drivers.cue.utils.cue_detection as cd
-from core.hal.drivers.driver import BaseDriver
+
+DEFAULT_MIN_SLEEP = 1e-4  # In seconds ~ 0.1ms
+
+DEFAULT_JSON_PATH = "core/calibration/calibration_data.json"
+DEFAULT_BKG_PATH = "home/background.jpg"
+
+DEFAULT_SIZE = 1920, 1080
+DEFAULT_MIN_MOMENT_00 = np.pi * 15 ** 2
+DEFAULT_MIN_DISTANCE = 10
+
+np.seterr(divide='ignore', invalid='ignore')
+
+@dataclass
+class Camera:
+    projection_matrix: np.ndarray
+    pool_focus_matrix: np.ndarray
+
+    def warp_projection(self, img: np.ndarray, size: tuple(int, int)) -> np.ndarray:
+        blue_chan = img[..., -1]
+        return cv2.warpPerspective(blue_chan, self.pool_focus_matrix, size, flags=cv2.INTER_LINEAR)
+
+    def project(self, point: np.ndarray) -> np.ndarray:
+        point = self.projection_matrix @ point
+        point = point / point[-1]
+        return point
+
+    @classmethod
+    def from_dict(cls, data: dict(str, Any)) -> "Camera":
+        projection_matrix = np.array(data["projection_matrix"])
+        poolFocus_matrix = np.array(data["poolFocus_matrix"])
+        return cls(projection_matrix, poolFocus_matrix)
+
+
+def CropCoords(lefty: float,righty: float) -> Any:
+    """
+    Crop lefty and righty values into Cartesian coordinates inside DEFAULT SIZE
+
+    Parameters:
+    lefty  (float): at abscissa    0, it is the left  ordinate of the line made by the cue
+    righty (float): at abscissa 1920, it is the right ordinate of the line made by the cue
+
+    Returns:
+    list(np.ndarray): a list of 2 Cartesian coordinnates (x, y). These coordinates are for sure inside the table rectangle (given by DEFAULT SIZE)
+    """
+
+    i=[0., float(lefty)]
+    j=[float(DEFAULT_SIZE[0]-1),float(righty)]
+
+    try:
+        # Calculate the coefficients. y=ax+b 
+        a = (j[1] - i[1]) / (j[0] - i[0])
+        b = i[1] - a * i[0]
+        if(lefty<0):
+            i=[(-b/a) -1, 0.]
+        elif(lefty > DEFAULT_SIZE[1]-1):
+            i=[((DEFAULT_SIZE[1]-b)/a) -1, float(DEFAULT_SIZE[1]-1)]
+        
+        if(righty<0):
+            j=[(-b/a) - 1, 0.]
+        elif(righty>DEFAULT_SIZE[1]-1):
+            j=[((DEFAULT_SIZE[1]-b)/a) -1, float(DEFAULT_SIZE[1] -1)]
+    except:
+        print("An exception occurred") 
+
+    i = np.array([int(i[0]), int(i[1]), 1])
+    j = np.array([int(j[0]), int(j[1]), 1])
+
+    return i,j
+
+
+def detect_cue(bkg: np.ndarray, frame: np.ndarray, camera: Camera) -> list(tuple(int, int)):
+    cue = False
+    # a,b = -1, -1
+    a1, a2, b1, b2 = -1,-1, -1, -1
+    
+    frame = cv2.absdiff(bkg, frame)
+    frame = camera.warp_projection(frame, DEFAULT_SIZE)
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    _, frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        lX=[x for [[x, _]] in c]
+        lY=[y for [[_, y]] in c]
+        if np.corrcoef(lX, lY)[0, 1]**2 > 0.83:
+            cue = True
+            [vx,vy,u,v] = cv2.fitLine(c, cv2.DIST_L2, 0, 0.01, 0.01)
+            #(0, lefty) and (1920, righty) draw the line made by the cue
+            lefty = (-u*vy/vx) + v
+            righty = ((frame.shape[1]-u)*vy/vx)+v
+
+            #convert lefty and righty into coordinates
+            a,b = CropCoords(lefty, righty)
+            # print(a, type(a))
+
+            #project
+            a1, a2, _ = camera.project(a)
+            b1, b2, _ = camera.project(b)
+            # print(a1, a2, b1, b2)
+    # return [cue, a, b]
+    return [cue, (int(a1), int(a2)), (int(b1), int(b2))]
+
+
+@dataclass
+class RuningMean:
+    value: float
+    step: int
+
+    def update(self, value: float) -> float:
+        self.value = self.value * self.step + value
+        self.step += 1
+        self.value = self.value / self.step
+        return self.value
+
 
 class Driver(BaseDriver):
-
-    def __init__(self, name: str, parent, max_fps: int = 120):
+    def __init__(self, name: str, parent, max_fps: int = 120) -> None:
         super().__init__(name, parent)
 
         self.register_to_driver("video", "color")
-
-        self.create_event("cue_data")
+        self.create_event("cue")
+        self.create_event("fps")
 
         self.fps = max_fps
+        self.stable_fps = RuningMean(60, 1)
+        self.history = []
 
-    def pre_run(self):
+    def pre_run(self) -> None:
         super().pre_run()
 
-        self.camera_data, self.empty_bg = cd.init()
+        with open(DEFAULT_JSON_PATH, 'r') as f:
+            data = json.load(f)
 
-    def loop(self):
+        self.bkg = cv2.imread(DEFAULT_BKG_PATH)
+        self.camera = Camera.from_dict(data)
+
+    def loop(self) -> None:
         start_t = time.time()
-        color = self.parent.get_driver_event_data("video","color")
-        if color is not None:
-            cue_data = cd.CueDetection(
-                self.empty_bg,
-                color, #to get a frame
-                self.camera_data
-            ) #it returns l
-            self.set_event_data("cue_data",cue_data)
 
-        dt = max(1 / self.fps - (time.time() - start_t), 0.0001)
-        time.sleep(dt)
+        frame = self.parent.get_driver_event_data("video", "color")
+        if frame is not None:
+            if self.bkg.shape != frame.shape:
+                self.bkg = cv2.resize(self.bkg, (frame.shape[1], frame.shape[0]))
+            cue = detect_cue(self.bkg, frame, self.camera)
+            # print(cue)
+            self.set_event_data("cue", cue)
+
+        dt = time.time() - start_t
+        time.sleep(max(1 / self.fps - dt, DEFAULT_MIN_SLEEP))
+
+        dt = time.time() - start_t
+        # fps = self.stable_fps.update(1 / dt)
+        fps = 1 / dt
+        if len(self.history) < 50:
+            self.history.append(fps)
+        else:
+            self.history.pop(0)
+            self.history.append(fps)
+
+        self.set_event_data("fps", f"{np.mean(self.history):.2f}")
