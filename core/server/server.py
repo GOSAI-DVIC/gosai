0067e020925790abce9621ef5789021168c862f6
Thomas <thomas.juldo@edu.devinci.fr>
2022-03-28 19:51:07 +0200

    Looking for platform name and creating base url from it
    Changed remote to control

--- core/server/server.py
+++ core/server/server.py
@@ -2,29 +2,39 @@ import os
 import threading
 from queue import Queue
 
+from dotenv import load_dotenv
 from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
-
-def start_chrome():
-    def _start_chrome():
+def start_chrome(path):
+    def _start_chrome(path):
         options = ["--start-fullscreen", "--incognito", "--disable-logging"]
         options_line = " ".join(options)
-        cmd = f"chromium http://127.0.0.1:5000/platform {options_line}"
+        cmd = f"chromium http://127.0.0.1:5000/{path}/platform {options_line}"
         os.system(f'/bin/bash -c "{cmd} &> /dev/null"')
 
-    threading.Thread(target=_start_chrome, args=(), daemon=True).start()
+    threading.Thread(target=_start_chrome, args=(path,), daemon=True).start()
 
 
 class Server:
+    """ Main server class """
     def __init__(self, hal):
         self.app = Flask(__name__)
         self.app.config["SECRET_KEY"] = "secret!"
         CORS(self.app)
 
+        load_dotenv("home/.env")
+        platform_name = os.getenv("PLATFORM")
+        if platform_name is None:
+            self.hal.log("Server: No platform name found in .env file", 3)
+            platform_name = "default"
+
+        base_path = f"gosai/{platform_name}"
+
         self.sio = SocketIO(
             self.app,
+            path= f"{base_path}/socket.io",
             logger=False,
             engineio_logger=False,
             async_mode="eventlet",
@@ -42,29 +52,31 @@ class Server:
                 self.sio.start_background_task(self.send_queued_data)
                 self.background_thread_started = True
 
-        @self.app.route("/")
+        @self.app.route("/gosai")
         def home():
             return "OK"
 
-        @self.app.route("/platform")
+        @self.app.route(f"/{base_path}/platform")
         def platform():
             return render_template("display/index.html")
 
-        @self.app.route("/platform/<path:path>")
+        @self.app.route(f"/{base_path}/platform/<path:path>")
         def platform_path(path):
             return send_from_directory("templates/display", path)
 
-        @self.app.route("/platform/home/<path:path>")
+        @self.app.route(f"/{base_path}/platform/home/<path:path>")
         def platform_home_path(path):
             return send_from_directory("../../home", path)
 
-        @self.app.route("/remote")
-        def remote():
+        @self.app.route(f"/{base_path}/control")
+        def control():
             return render_template("control/index.html")
 
-        @self.app.route("/remote/<path:path>")
+        @self.app.route(f"/{base_path}/control/<path:path>")
         def control_path(path):
             return send_from_directory("templates/control", path)
+
+        self.path = base_path
 
 
     def start(self, socket_port=5000):
