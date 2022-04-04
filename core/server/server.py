bdf1e3ab647c95f5ec53b4bdaf3d2d8af0ce4d7b
Maxime <mbroussart@gmail.com>
2022-04-04 18:16:49 +0200

    Added control password generation and API

--- core/server/server.py
+++ core/server/server.py
@@ -1,9 +1,12 @@
+import datetime as dt
 import os
+import random
 import threading
+import time
 from queue import Queue
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
@@ -46,11 +49,30 @@ class Server:
         self.queue = Queue()
         self.background_thread_started = False
 
+        self.control_password = "314159"
+
         @self.sio.on("connect")
         def connect(*args):
             if not self.background_thread_started:
                 self.sio.start_background_task(self.send_queued_data)
                 self.background_thread_started = True
+
+        @self.sio.on("get_control_password")
+        def get_control_password():
+            self.sio.emit("control_password", { "control_password" :self.control_password}, room=request.sid)
+
+        @self.sio.on("generate_control_password")
+        def generate_control_password():
+            self.generate_control_password()
+            print("--control_password : " + self.control_password)
+            self.sio.emit("control_password", { "control_password" :self.control_password}, room=request.sid)
+
+        @self.sio.on("check_control_password")
+        def check_control_password(data : dict):
+            check = False
+            if (data["control_password"] == self.control_password):
+                check = True
+            self.sio.emit("checked_control_password", {"checked" : check}, room=request.sid)
 
         @self.app.route("/gosai")
         def home():
@@ -76,14 +98,41 @@ class Server:
         def control_path(path):
             return send_from_directory("templates/control", path)
 
+        @self.app.route(f"/{base_path}/core/<path:path>")
+        def core_path(path):
+            return send_from_directory("../", path)        
+
         self.path = base_path
 
+    def daily_control_password_generator(self):
+        """Generate daily a 6 digits password to access /control"""
+
+        def _daily_control_password_generator(self):
+            self.control_password = str(random.randint(0, 999999)).zfill(6)
+            time.sleep(_waitToTomorrow())
+        
+        def _waitToTomorrow():
+            '''Wait to tomorrow 00:00 am.'''
+            tomorrow = dt.datetime.replace(dt.datetime.now() + dt.timedelta(days=1),
+                        hour=0,minute=0,second=0)
+            delta = tomorrow - dt.datetime.now()
+            return delta.seconds
+
+        threading.Thread(
+            target= _daily_control_password_generator, args=(self,), daemon=True
+        ).start()
+
+    def generate_control_password(self):
+        """Generate a 6 digits password to access /control"""
+        self.control_password = str(random.randint(0, 999999)).zfill(6)
 
     def start(self, socket_port=8000):
         """Starts the server on the given port"""
 
         def _start_socket(self, port):
             self.sio.run(self.app, host="0.0.0.0", port=port)
+        
+        self.daily_control_password_generator()
 
         threading.Thread(
             target=_start_socket, args=(self, socket_port), daemon=True
