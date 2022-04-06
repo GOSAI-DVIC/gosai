fb0b6988c8d510b77c29b06f9d49ae8870c7f8e6
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-06 18:39:07 +0200

    Adding unique system logger

---
+++ core/logs/logger.py
@@ -0,0 +1,77 @@
+import datetime
+import locale
+import os
+import pickle
+import threading
+
+import redis
+
+LOGS_PATH = "core/logs"
+locale.setlocale(locale.LC_ALL, "")
+
+
+class Logger:
+    """
+    Logger class
+    """
+
+    def __init__(self, server):
+        """
+        Constructor and API for the logger
+        """
+        self.server = server
+        self.db = redis.Redis(host="localhost", port=6379, db=0)
+
+        self.history = []
+
+        @self.server.sio.on("get_log_history")
+        def _():
+            self.server.send_data("log_history", {
+                "history": self.history
+            })
+
+    def log(self, source, content, level=1):
+        """
+        Logs a message
+        """
+
+        log = {
+            "time": datetime.datetime.now().strftime('%b-%d-%G-%I:%M:%S%p'),
+            "source": source,
+            "content": content,
+            "level": level
+        }
+        if level >= int(os.environ["LOG_LEVEL"]):
+            print(f"{log['level']} : {log['source']} : {log['content']}")
+            self.history.append(log)
+            self.server.send_data("log", log)
+
+        if level >= 2:
+            with open(f"{LOGS_PATH}/{source}.log", "a+") as log_file:
+                log_file.write(
+                    f"{log['level']} : {log['time']} : {log['content']}\n"
+                )
+
+    def log_listenner(self):
+        """
+        Listens for logs comming from the redis server
+        """
+
+        def _log_listenner(self):
+            ps = self.db.pubsub()
+            ps.subscribe("log")
+            for binary_data in ps.listen():
+                try:
+                    data = pickle.loads(bytes(binary_data["data"]))
+                    self.log(data["source"], data["content"], data["level"])
+                except Exception as e:
+                    self.log("logger", f"Error while loading logged data: {e}", 3)
+                    pass
+
+        threading.Thread(target=_log_listenner, args=(self,)).start()
+
+    def log_to_string(log):
+        """
+        Logs a message to a string
+        """
+        return f"{log['source']}: {log['content']}"
