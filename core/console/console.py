3e38a14ab887219177cf56439d58bd9a26a7a18e
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-06 18:49:48 +0200

    Sleeping before reading command for proper spacing

--- core/console/console.py
+++ core/console/console.py
@@ -14,6 +14,7 @@ class Console(threading.Thread):
         time.sleep(1)
         print("")
         while 1:
+            time.sleep(0.5)
             print("")
             command = input(">>: ")
             execute = command.split()[0]
