5cad7d1a7141fea92ffae8c2f4e199ca8118b02b
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-08 03:40:46 +0200

    Printing through logs, better commands and remote execution

--- core/console/console.py
+++ core/console/console.py
@@ -1,14 +1,25 @@
 import os
+import pickle
+import sys
 import threading
 import time
 
+import redis
+
+
 class Console(threading.Thread):
-    """ A console to interract with the OS """
+    """A console to interract with the OS"""
+
     def __init__(self, hal, server, app_manager):
         threading.Thread.__init__(self)
         self.hal = hal
         self.server = server
         self.app_manager = app_manager
+        self.db = redis.Redis(host="localhost", port=6379, db=0)
+
+        @self.server.sio.on("execute_command")
+        def _(data) -> None:
+            eval_command(self, data["command"])
 
     def run(self):
         time.sleep(1)
@@ -16,79 +27,112 @@ class Console(threading.Thread):
         while 1:
             time.sleep(0.5)
             print("")
-            command = input(">>: ")
-            execute = command.split()[0]
-            arguments = command.split()[1:]
+            command = input()
+            eval_command(self, command)
 
-            if execute == "exit":
-                exit()
-            elif execute == "help":
-                print("Available commands:")
-                print("exit - exit the application")
-                print("help - show this help")
-                print("ls - list all available drivers")
-                print("log $level - set the log level")
-                print("restart - restart an app")
-                print("start $app_name - start an app")
-                print("stop $app_name - stop an app")
-                #print("status - show the status of an app")
-                print("ps - displays informations of the drivers")
+    def log(self, content, level=2):
+        """Logs via the redis database"""
+        data = {"source": "console", "content": content, "level": level}
+        self.db.set(f"log", pickle.dumps(data))
+        self.db.publish(f"log", pickle.dumps(data))
 
-            elif execute == "start":
-                if len(arguments) == 0:
-                    print("Please specify an application to start")
-                else:
-                    self.app_manager.start(arguments[0])
 
-            elif execute == "stop":
-                if len(arguments) == 0:
-                    print("Please specify an application to stop")
-                else:
-                    self.app_manager.stop(arguments[0])
+def eval_command(console: Console, command: str) -> None:
+    execute = command.split()[0]
+    arguments = command.split()[1:]
 
-            elif execute == "restart":
-                if len(arguments) == 0:
-                    print("Please specify an application to restart")
-                elif len(arguments) == 1:
-                    self.app_manager.stop(arguments[0])
-                    self.app_manager.start(arguments[0])
+    if execute == "exit":
+        console.log("Exiting...")
+        sys.exit()
 
-            elif execute == "exit":
-                exit()
+    elif execute == "help":
+        result = "Available commands:\n"
+        result += "    exit - exit the application\n"
+        result += "    help - show this help\n"
+        result += "    ls drivers - list all available drivers\n"
+        result += "    ls applications - list all available applications\n"
+        result += "    log $level - set the log level\n"
+        result += "    restart $app_name - restart an app\n"
+        result += "    start $app_name - start an app\n"
+        result += "    stop $app_name - stop an app\n"
+        console.log(result)
 
-            elif execute == "ls":
-                if len(arguments) == 1 and arguments[0] == "drivers":
-                    print("Available drivers:")
-                    print("\n".join(self.hal.get_drivers()))
+    elif execute == "start":
+        if len(arguments) == 0:
+            console.log("Please specify an application to start", 3)
+        else:
+            console.app_manager.start(arguments[0])
 
-                if len(arguments) == 2 and arguments[0] == "drivers" and arguments[1] == "started":
-                    print("Started drivers:")
-                    print("\n".join(self.hal.get_started_drivers()))
+    elif execute == "stop":
+        if len(arguments) == 0:
+            console.log("Please specify an application to stop", 3)
+        else:
+            console.app_manager.stop(arguments[0])
 
-                if len(arguments) == 2 and arguments[0] == "drivers" and arguments[1] == "stopped":
-                    print("Stopped drivers:")
-                    print("\n".join(self.hal.get_stopped_drivers()))
+    elif execute == "restart":
+        if len(arguments) == 0:
+            console.log("Please specify an application to restart", 3)
+        elif len(arguments) == 1:
+            console.app_manager.stop(arguments[0])
+            console.app_manager.start(arguments[0])
 
-                if len(arguments) == 1 and arguments[0] == "applications":
-                    print("Available applications:")
-                    print("\n".join(self.app_manager.list_applications()))
+    elif execute == "ls":
+        if len(arguments) == 1 and arguments[0] == "drivers":
+            result = "Available drivers:\n"
+            result += "\n".join(console.hal.get_drivers()) + "\n"
+            console.log(result)
 
-                if len(arguments) == 2 and arguments[0] == "applications" and arguments[1] == "started":
-                    print("Started applications:")
-                    print("\n".join(self.app_manager.list_started_applications()))
+        if (
+            len(arguments) == 2
+            and arguments[0] == "drivers"
+            and arguments[1] == "started"
+        ):
+            result = "Started drivers:\n"
+            result += "\n".join(console.hal.get_started_drivers()) + "\n"
+            console.log(result)
 
-                if len(arguments) == 2 and arguments[0] == "applications" and arguments[1] == "stopped":
-                    print("Stopped applications:")
-                    print("\n".join(self.app_manager.list_stopped_applications()))
+        if (
+            len(arguments) == 2
+            and arguments[0] == "drivers"
+            and arguments[1] == "stopped"
+        ):
+            result = "Stopped drivers:\n"
+            result += "\n".join(console.hal.get_stopped_drivers()) + "\n"
+            console.log(result)
 
-            elif execute == "log":
-                if len(arguments) == 1:
-                    try:
-                        os.environ["LOG_LEVEL"] = str(int(arguments[0]))
-                    except:
-                        print("Invalid log level")
-                else:
-                    print("Please specify a log level")
+        if len(arguments) == 1 and arguments[0] == "applications":
+            result = "Available applications:\n"
+            result += "\n".join(console.app_manager.list_applications()) + "\n"
+            console.log(result)
 
-            else:
-                print("Unknown command")
+        if (
+            len(arguments) == 2
+            and arguments[0] == "applications"
+            and arguments[1] == "started"
+        ):
+            result = "Started applications:\n"
+            result += "\n".join(console.app_manager.list_started_applications()) + "\n"
+            console.log(result)
+
+        if (
+            len(arguments) == 2
+            and arguments[0] == "applications"
+            and arguments[1] == "stopped"
+        ):
+            result = "Stopped applications:\n"
+            result += "\n".join(console.app_manager.list_stopped_applications()) + "\n"
+            console.log(result)
+
+    elif execute == "log":
+        if len(arguments) == 1:
+            try:
+                assert int(arguments[0]) in range(0, 3)
+                os.environ["LOG_LEVEL"] = str(int(arguments[0]))
+                console.log("Log level set to " + arguments[0], 3)
+            except Exception as e:
+                console.log("Invalid log level: " + str(e))
+        else:
+            console.log("Please specify a log level", 3)
+
+    else:
+        console.log("Unknown command: " + command, 3)
