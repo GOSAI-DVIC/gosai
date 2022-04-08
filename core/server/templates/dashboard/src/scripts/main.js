150cbca2e1f7243ec104a8302f157a3c014ed9d4
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-08 03:52:23 +0200

    Sending commands when Enter or button is pressed and adding stats

--- core/server/templates/dashboard/src/scripts/main.js
+++ core/server/templates/dashboard/src/scripts/main.js
@@ -5,19 +5,41 @@ function generate_console_logs() {
     history.forEach(log => {
         logs += log_to_str(log);
     });
-    document.getElementById("console-output").innerText = logs;
+    document.getElementById("console-output").innerHTML = logs;
+    document.getElementById("console-output").scrollTop = document.getElementById("console-output").scrollHeight;
 }
 
 function add_to_logs(log) {
     history.push(log);
-    document.getElementById("console-output").innerText += log_to_str(log);
+    document.getElementById("console-output").innerHTML += log_to_str(log);
+    document.getElementById("console-output").scrollTop = document.getElementById("console-output").scrollHeight;
 }
 
 function log_to_str(data) {
-    return data["time"] + " : " + data["level"] + " : " + data["source"] + " : " + data["content"] + "\n";
+    let level_color = "";
+    switch (data["level"]) {
+        case 3:
+            level_color = "level-warning";
+            break;
+        case 4:
+            level_color = "level-error";
+            break;
+        default:
+            level_color = "level-info";
+            break;
+    }
+    let result = "<span class='" + level_color + "'>" + data["time"] + " : " + data["level"] + " : " + data["source"] + "</span> : " + data["content"] + "<br>";
+    return result.replace(/\n/g, "<br />");
 }
 
 function send_command() {
-    let command = document.getElementById("command-input").value;
-    socket.emit("command", command);
+    let command = document.getElementById("console-input").value;
+    socket.emit("execute_command", {"command": command});
+    document.getElementById("console-input").value = "";
+}
+
+function do_send(event) {
+    if(event.key === 'Enter') {
+        send_command();
+    }
 }
--- core/server/templates/dashboard/src/scripts/socket.js
+++ core/server/templates/dashboard/src/scripts/socket.js
@@ -1,26 +1,75 @@
-let path_parameter = window.location.pathname.split('/');
+let path_parameter = window.location.pathname.split("/");
 path_parameter.splice(-1);
-const path = path_parameter.join('/');
+const path = path_parameter.join("/");
 
 const socket = io.connect(window.location.origin, {
     path: path + "/socket.io",
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
     },
-	agent: false,
-	upgrade: false,
-	rejectUnauthorized: false,
-    transports: ["websocket"]
+    agent: false,
+    upgrade: false,
+    rejectUnauthorized: false,
+    transports: ["websocket"],
 });
 
-socket.on("log_history", data => {
+socket.on("log_history", (data) => {
     history = data["history"];
     generate_console_logs();
-})
+});
 
-socket.on("log", log => {
+socket.on("log", (log) => {
     add_to_logs(log);
 });
 
+socket.on("available_applications", (data) => {
+    let applications = data["applications"];
+    let display = document.getElementById("applications-stats-available");
+    display.innerText = applications.length;
+});
+
+socket.on("started_applications", (data) => {
+    let applications = data["applications"];
+    let display = document.getElementById("applications-stats-started");
+    display.innerText = applications.length;
+});
+
+socket.on("stopped_applications", (data) => {
+    let applications = data["applications"];
+    let display = document.getElementById("applications-stats-stopped");
+    display.innerText = applications.length;
+});
+
+socket.on("available_drivers", (data) => {
+    let drivers = data["drivers"];
+    let display = document.getElementById("drivers-stats-available");
+    display.innerText = drivers.length;
+});
+
+socket.on("started_drivers", (data) => {
+    let drivers = data["drivers"];
+    let display = document.getElementById("drivers-stats-started");
+    display.innerText = drivers.length;
+});
+
+socket.on("stopped_drivers", (data) => {
+    let drivers = data["drivers"];
+    let display = document.getElementById("drivers-stats-stopped");
+    display.innerText = drivers.length;
+});
+
+socket.on("connected_users", (data) => {
+    let clients = data["users"];
+    let display = document.getElementById("clients-stats-connected");
+    display.innerText = Object.keys(clients).length;
+});
+
 socket.emit("get_log_history");
+socket.emit("get_available_applications");
+socket.emit("get_started_applications");
+socket.emit("get_stopped_applications");
+socket.emit("get_available_drivers");
+socket.emit("get_started_drivers");
+socket.emit("get_stopped_drivers");
+socket.emit("get_users");
