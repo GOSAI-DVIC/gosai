babd8e5571cef2783489a848e1312a14c69d613c
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-11 21:57:20 +0200

    Adding performance monitoring

---
+++ core/system_monitor/system_monitor.py
@@ -0,0 +1,91 @@
+# record : {"source": {"name": str, "type": str}, "type": str, "data": Any }
+
+
+import datetime
+import locale
+import os
+import pickle
+import threading
+import time
+
+import redis
+
+locale.setlocale(locale.LC_ALL, "")
+
+class Monitor:
+    """
+    System monitor class
+    """
+
+    def __init__(self, server):
+        """
+        Constructor and API for the system monitor
+        """
+        self.name = "system_monitor"
+        self.server = server
+        self.db = redis.Redis(host="localhost", port=6379, db=0)
+
+        self.recent_performances = {}
+        self.display_stats = {}
+
+        @self.server.sio.on("get_recent_performances")
+        def _():
+            self.server.send_data("recent_performances", {
+                "performances": self.recent_performances
+            })
+
+        @self.server.sio.on("purge_recent_performances")
+        def _():
+            self.recent_performances = {}
+
+        @self.server.sio.on("set_display_statistics")
+        def set_display_statistics(data):
+            self.display_stats = data
+            self.server.sio.emit("display_statistics", data)
+
+        @self.server.sio.on("get_display_statistics")
+        def get_display_statistics():
+            self.server.sio.emit("display_statistics", self.display_stats)
+
+
+
+    def record_listenner(self):
+        """
+        Listens for performance records comming from the redis server
+        """
+
+        def _record_listenner(self):
+            ps = self.db.pubsub()
+            ps.subscribe("performance_record")
+            for binary_data in ps.listen():
+                try:
+                    data = pickle.loads(bytes(binary_data["data"]))
+                    rec_src_name = data["source"]["name"]
+                    rec_src_type = data["source"]["type"]
+                    rec_type = data["type"]
+                    rec_data = data["data"]
+
+                    if rec_src_type not in self.recent_performances:
+                        self.recent_performances[rec_src_type] = {}
+                    if rec_src_name not in self.recent_performances[rec_src_type]:
+                        self.recent_performances[rec_src_type][rec_src_name] = {}
+                    self.recent_performances[rec_src_type][rec_src_name][rec_type] = rec_data
+
+                except Exception as e:
+                    self.log(f"Error while loading performance data: {e}", 3)
+                    pass
+
+
+        thread = threading.Thread(target=_record_listenner, args=(self,))
+        thread.start()
+
+
+    def log(self, content, level=1):
+        """Logs via the redis database"""
+        data = {
+            "source": self.name,
+            "content": content,
+            "level": level
+        }
+        self.db.set("log", pickle.dumps(data))
+        self.db.publish("log", pickle.dumps(data))
--- init.py
+++ init.py
@@ -3,6 +3,7 @@ import os
 from core.console.console import Console
 from core.hal.hal import HardwareAbstractionLayer
 from core.logs.logger import Logger
+from core.system_monitor.system_monitor import Monitor
 from core.manager.app_manager import AppManager
 from core.server.server import Server, start_chrome
 
@@ -15,6 +16,9 @@ hal = HardwareAbstractionLayer(server)
 
 logger = Logger(server)
 logger.log_listenner()
+
+monitor = Monitor(server)
+monitor.record_listenner()
 
 app_manager = AppManager(hal, server)
 app_manager.start("menu")
