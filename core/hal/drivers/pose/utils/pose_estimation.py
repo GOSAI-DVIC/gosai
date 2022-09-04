db626cbf468cf034ca7a818dcab9cbf5dac89126
Thomas-Jld <thomas.juldo@gmail.com>
2022-09-04 21:14:17 +0200

    Normalized values + ea

--- core/hal/drivers/pose/utils/pose_estimation.py
+++ core/hal/drivers/pose/utils/pose_estimation.py
@@ -13,6 +13,30 @@ def init():
         model_complexity=0,
     )
 
+def landmarks_to_array(landmarks, window=1):
+    landmark_array = [
+        [
+            (0.5 - window / 2) + landmark.x * window,
+            landmark.y,
+            round(landmark.visibility, 2),
+        ]
+        for landmark in landmarks
+    ]
+    return landmark_array
+
+
+def world_landmarks_to_array(landmarks, window=1):
+    landmark_array = [
+        [
+            landmark.x,
+            landmark.y,
+            landmark.z,
+            round(landmark.visibility, 2),
+        ]
+        for landmark in landmarks
+    ]
+    return landmark_array
+
 
 def find_all_poses(holistic, frame, window):
     # start = time.time()
@@ -36,60 +60,17 @@ def find_all_poses(holistic, frame, window):
     # e2 = time.time()
     # print(f"    Infer image: {(e2 - e1)*1000} ms")
 
-    body_landmarks = []
 
-    if results.pose_landmarks:
-        body_landmarks = [
-            [
-                (0.5 - window / 2) + landmark.x * window,
-                landmark.y,
-                round(landmark.visibility, 2),
-            ]
-            for landmark in results.pose_landmarks.landmark
-        ]
-
-    faces_landmarks = []
-
-    if results.face_landmarks:
-        faces_landmarks = [
-            [
-                (0.5 - window / 2) + landmark.x * window,
-                landmark.y,
-                round(landmark.visibility, 2),
-            ]
-            for landmark in results.face_landmarks.landmark
-        ]
-
-    left_hands_landmarks = []
-
-    if results.left_hand_landmarks:
-        left_hands_landmarks = [
-            [
-                (0.5 - window / 2) + landmark.x * window,
-                landmark.y,
-                round(landmark.visibility, 2),
-            ]
-            for landmark in results.left_hand_landmarks.landmark
-        ]
-
-    right_hands_landmarks = []
-
-    if results.right_hand_landmarks:
-        right_hands_landmarks = [
-            [
-                (0.5 - window / 2) + landmark.x * window,
-                landmark.y,
-                round(landmark.visibility, 2),
-            ]
-            for landmark in results.right_hand_landmarks.landmark
-        ]
-
-    # e3 = time.time()
-    # print(f"    Convert data: {(e3 - e2)*1000} ms")
+    face_landmarks = landmarks_to_array(results.face_landmarks.landmark, window) if results.face_landmarks else []
+    body_landmarks = landmarks_to_array(results.pose_landmarks.landmark, window) if results.pose_landmarks else []
+    left_hand_landmarks = landmarks_to_array(results.left_hand_landmarks.landmark, window) if results.left_hand_landmarks else []
+    right_hand_landmarks = landmarks_to_array(results.right_hand_landmarks.landmark, window) if results.right_hand_landmarks else []
+    body_wolrd_landmarks = world_landmarks_to_array(results.pose_world_landmarks.landmark, window) if results.pose_world_landmarks else []
 
     return {
-        "face_mesh": faces_landmarks,
-        "body_pose": body_landmarks,
-        "right_hand_pose": left_hands_landmarks,
-        "left_hand_pose": right_hands_landmarks,
+        "face_mesh": face_landmarks, # [[x, y, visibility]]
+        "body_pose": body_landmarks, # [[x, y, visibility]]
+        "right_hand_pose": left_hand_landmarks, # [[x, y, visibility]]
+        "left_hand_pose": right_hand_landmarks, # [[x, y, visibility]]
+        "body_world_pose": body_wolrd_landmarks, # [[x, y, z, visibility]]
     }
