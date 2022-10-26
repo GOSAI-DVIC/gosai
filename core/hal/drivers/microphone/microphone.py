9f0475bec92d24640dfdea4d9b3a8943cab724bf
AdrLfv_mirror <adrien.lefevre@edu.devinci.fr>
2022-10-26 18:16:22 +0200

    Checking mic idx in config

--- core/hal/drivers/microphone/microphone.py
+++ core/hal/drivers/microphone/microphone.py
@@ -1,8 +1,10 @@
+import json
 import math
 from core.hal.drivers.driver import BaseDriver
 import sounddevice as sd
 import queue
 import numpy as np
+from os import path
 
 
 import sounddevice as sd
@@ -21,8 +23,19 @@ class Driver(BaseDriver):
         # runs to do at the start of the driver
         BLOCKSIZE = 1024  # TODO:read these parameters from config.json
         SAMPLERATE = 48000
-        CHANNELS = 1
-        DEVICE = 6
+
+        if path.exists("home/config.json"):
+            with open("home/config.json", "r") as f:
+                config = json.load(f)
+                if ("channels_nb" in config["microphone"]):
+                    CHANNELS = config["microphone"]["channels_nb"]
+        
+                if ("number" in config["microphone"]):
+                    DEVICE = config["microphone"]["number"]
+        
+        else: 
+            CHANNELS = 1
+            DEVICE = 6
 
         def callback(indata, frames, time, status):
             self.set_event_data(
@@ -32,7 +45,6 @@ class Driver(BaseDriver):
                     "samplerate": SAMPLERATE,
                 },
             )
-
         stream = sd.InputStream(
             samplerate=SAMPLERATE,
             blocksize=BLOCKSIZE,
