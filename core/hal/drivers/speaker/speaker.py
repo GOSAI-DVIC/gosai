2567058e029573e60d2cb6a4396d0074119a3d9d
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2023-09-08 00:33:24 +0200

    Improved speaker

--- core/hal/drivers/speaker/speaker.py
+++ core/hal/drivers/speaker/speaker.py
@@ -23,38 +23,51 @@ class Driver(BaseDriver):
     def pre_run(self):
         super().pre_run()
 
+        self.create_event("settings")
+        self.create_callback("play", self.play)
+        self.create_callback("empty_buffer", self.empty_buffer)
+
+        DEVICE, SAMPLERATE, CHANNELS, BLOCKSIZE = get_speaker_configuration(self.parent.config)
+        self.set_event_data("settings", {
+            "device": DEVICE,
+            "samplerate": SAMPLERATE,
+            "channels": CHANNELS,
+            "blocksize": BLOCKSIZE,
+        })
+
         self.buffer = queue.Queue()
 
         def callback(outdata, frames, time, status):
-            try:
-                data = self.buffer.get_nowait()
-            except queue.Empty:
-                data = np.zeros(frames, dtype=np.float32)
-            if len(data) < len(outdata):
-                outdata[:len(data)] = np.array(data).reshape(-1,1)
-                outdata[len(data):] = np.zeros(len(outdata) - len(data)).reshape(-1,1)
+            if not self.buffer.empty():
+                data = self.buffer.get()
             else:
-                outdata[:] = np.array(data).reshape(-1,1)
+                data = np.zeros(frames, dtype=np.float32)
+            outdata[:] = np.array(data).reshape(-1,1)
 
 
-        DEVICE, SAMPLERATE, CHANNELS, BLOCKSIZE = get_speaker_configuration(self.parent.config)
         self.blocksize = BLOCKSIZE
 
         stream = sd.OutputStream(
             samplerate=SAMPLERATE, blocksize = BLOCKSIZE, callback=callback, channels=CHANNELS, device=DEVICE)
         stream.start()
 
-        self.create_callback("play", self.play)
-        self.create_callback("empty_buffer", self.empty_buffer)
+    def play(self, data: np.ndarray):
+        """
+        Add the given data to the play buffer. If the data is smaller than the blocksize, it will be padded with zeros!
+        """
+        if len(data.shape) > 1:
+            data = data.reshape(-1)
 
-    def play(self, data: List):
-        """
-        Plays the given data
-        """
-        blocks = len(data) // self.blocksize
-        for i in range(blocks):
-            self.buffer.put_nowait(data[i*self.blocksize:(i+1)*self.blocksize])
-        self.buffer.put_nowait(data[blocks*self.blocksize:])
+        i = 0
+        data_size = data.shape[0]
+
+        while i < data_size:
+            size = min(data_size - i, self.blocksize )
+            b = data[i:i+size]
+            if size < self.blocksize:
+                b = np.append(b, np.zeros(self.blocksize - size))
+            self.buffer.put(b)
+            i += size
 
     def empty_buffer(self):
         """
