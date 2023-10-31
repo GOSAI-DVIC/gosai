069f524a6c1511e2f478253d943569cb6d615496
Hugo <hugo.daumain@edu.devinci.fr>
2023-10-31 20:48:39 +0100

    changing_model

--- core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
+++ core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
@@ -2,7 +2,7 @@ from core.hal.drivers.driver import BaseDriver
 import numpy as np
 from scipy.signal import resample
 import numpy as np 
-from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model, pipeline
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model, pipeline, AutoConfig, AutoProcessor, AutoModel
 import torch
 import torch.nn.functional as F
 # import webrtcvad
@@ -18,26 +18,33 @@ class Driver(BaseDriver):
         super().pre_run()
         self.device = 'cpu'
         self.debug = False
-        model_name_or_path = "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition"
-        #self.config = Wav2Vec2Config.from_pretrained(model_name_or_path)
+        model_name_or_path = "antonjaragon/emotions_xls53_6_classes"
+
+        
+        self.config = AutoConfig.from_pretrained(model_name_or_path)
+
+        #self.processor = AutoProcessor.from_pretrained(model_name_or_path)
         self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
-        self.sampling_rate = self.feature_extractor.sampling_rate
+        #self.sampling_rate = self.feature_extractor.sampling_rate
         self.model = Wav2Vec2Model.from_pretrained(model_name_or_path).to(self.device)
 
         self.pipelineclassifier = pipeline("audio-classification", model=model_name_or_path)
         self.create_callback("predict", self.predict)
 
+        #print(self.pipelineclassifier.config)
 
     def predict(self, data : dict):
         """
         data["activity_buffer"] : audio list
             -> float32
-            -> 16000Hz
+            
         
         """
         ## Outputs activity probablitily
         data = np.array(data['audio_buffer'])
-
+        #data = self.feature_extractor(data)
+        #data = {"inputs" : data, "attention_mask" : np.array([1 for i in range(len(data))])}
+        
 
         #inputs = self.feature_extractor(data, sampling_rate = self.sampling_rate)
         
