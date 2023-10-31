9d5bf1d1052b8b00577b22a58f1b42736582b990
Hugo <hugo.daumain@edu.devinci.fr>
2023-10-31 14:04:09 +0100

    update_emo_module

--- core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
+++ core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
@@ -2,7 +2,7 @@ from core.hal.drivers.driver import BaseDriver
 import numpy as np
 from scipy.signal import resample
 import numpy as np 
-from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForSpeechClassification, AutoConfig
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model, pipeline
 import torch
 import torch.nn.functional as F
 # import webrtcvad
@@ -18,11 +18,13 @@ class Driver(BaseDriver):
         super().pre_run()
         self.device = 'cpu'
         self.debug = False
-        model_name_or_path = 'ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition'
-        self.config = AutoConfig.from_pretrained(model_name_or_path)
+        model_name_or_path = "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition"
+        #self.config = Wav2Vec2Config.from_pretrained(model_name_or_path)
         self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
         self.sampling_rate = self.feature_extractor.sampling_rate
-        self.model = Wav2Vec2ForSpeechClassification.from_pretrained(model_name_or_path).to(device)
+        self.model = Wav2Vec2Model.from_pretrained(model_name_or_path).to(self.device)
+
+        self.pipelineclassifier = pipeline("audio-classification", model=model_name_or_path)
         self.create_callback("predict", self.predict)
 
 
@@ -34,17 +36,30 @@ class Driver(BaseDriver):
         
         """
         ## Outputs activity probablitily
-        inputs = self.feature_extractor(data, sampling_rate = self.sampling_rate)
-        inputs = {key: inputs[key].to(self.device) for key in inputs}
+        data = np.array(data['audio_buffer'])
+
+
+        #inputs = self.feature_extractor(data, sampling_rate = self.sampling_rate)
+        
+        #inputs = {key: torch.Tensor(inputs[key][0]) for key in inputs}
+        #inputs = {"inputs" : data }
+
+        #print(inputs.shape)
+
+  
         with torch.no_grad():
-            logits = self.model(**inputs).logits
-        scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0]
-        outputs = [{"Emotion": self.config.id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in enumerate(scores)]
+            outputs = self.pipelineclassifier(data)
+
+
+        #scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0]
+
+        #outputs = [{"Emotion": self.config.id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in enumerate(scores)]
+
         #Send it back to the application
         self.set_event_data(
             "speech_emotion",
             {
-                "results": outputs,
+               "results": outputs,
             },
         )
         #return speech_prob
