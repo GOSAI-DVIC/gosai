ef99a847ff53bd119cef8efd7226781981aa9461
Hugo <hugo.daumain@edu.devinci.fr>
2023-12-11 11:04:39 +0100

    updating_drivers_doc

--- core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
+++ core/hal/drivers/speech_emo_extraction/speech_emo_extraction.py
@@ -23,52 +23,38 @@ class Driver(BaseDriver):
         
         self.config = AutoConfig.from_pretrained(model_name_or_path)
 
-        #self.processor = AutoProcessor.from_pretrained(model_name_or_path)
+   
         self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
-        #self.sampling_rate = self.feature_extractor.sampling_rate
+
         self.model = Wav2Vec2Model.from_pretrained(model_name_or_path).to(self.device)
 
         self.pipelineclassifier = pipeline("audio-classification", model=model_name_or_path)
         self.create_callback("predict", self.predict)
 
-        #print(self.pipelineclassifier.config)
+   
 
     def predict(self, data : dict):
         """
         data["activity_buffer"] : audio list
             -> float32
             
-        
         """
-        ## Outputs activity probablitily
+      
         data = np.array(data['audio_buffer'])
-        #data = self.feature_extractor(data)
-        #data = {"inputs" : data, "attention_mask" : np.array([1 for i in range(len(data))])}
-        
-
-        #inputs = self.feature_extractor(data, sampling_rate = self.sampling_rate)
-        
-        #inputs = {key: torch.Tensor(inputs[key][0]) for key in inputs}
-        #inputs = {"inputs" : data }
-
-        #print(inputs.shape)
 
   
         with torch.no_grad():
             outputs = self.pipelineclassifier(data)
 
 
-        #scores = F.softmax(logits, dim=1).detach().cpu().numpy()[0]
-
-        #outputs = [{"Emotion": self.config.id2label[i], "Score": f"{round(score * 100, 3):.1f}%"} for i, score in enumerate(scores)]
-
-        #Send it back to the application
         self.set_event_data(
             "speech_emotion",
             {
                "results": outputs,
             },
         )
-        #return speech_prob
+        """
+        outputs is a dict with all emotions associated with their scores
+        """
 
 
--- core/hal/drivers/speech_to_text/speech_to_text.py
+++ core/hal/drivers/speech_to_text/speech_to_text.py
@@ -24,15 +24,16 @@ class Driver(BaseDriver):
         self.fast_whisper_bool = False
         self.path_dir = 'core/hal/drivers/speech_to_text/models'
         self.model_size = 'medium.en'
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         if self.model_str == 'faster_whisper' :
            
-            self.pipe = WhisperModel(model_size_or_path = '/home/hugo/Desktop/Project/gosai/core/hal/drivers/speech_to_text/models/models--Systran--faster-whisper-medium.en/snapshots/a29b04bd15381511a9af671baec01072039215e3', device="cuda", compute_type="int8")
+            self.pipe = WhisperModel(model_size_or_path = '/home/hugo/Desktop/Project/gosai/core/hal/drivers/speech_to_text/models/models--Systran--faster-whisper-medium.en/snapshots/a29b04bd15381511a9af671baec01072039215e3', device=self.device, compute_type="int8")
             
         else : 
 
 
-            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+            
             self.log(self.device, 3)
             torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
             model_id = "distil-whisper/distil-medium.en"
@@ -49,7 +50,7 @@ class Driver(BaseDriver):
                 torch_dtype=torch_dtype,
                 device=self.device,
             )
-        #self.model = WhisperModel("medium.en", device="cuda", compute_type="int8")
+     
         self.create_callback("transcribe", self.transcribe)
 
 
@@ -62,25 +63,16 @@ class Driver(BaseDriver):
         start = time.time()
         audio = data["audio_buffer"]
 
-      
-            #Save last audio buffer to file for debugging 
-           
-       # self.log("transcription",3)
-
         if self.model_str=='faster_whisper' :
             segments, info = self.pipe.transcribe(np.array(audio), beam_size = 5)
             txt = ''
             for segment in segments : 
                 txt = txt + segment.text 
 
-
         else :
             with torch.cuda.device(self.device):
                 txt = self.pipe(np.array(audio))['text']
 
-        #self.log("self.model.transcribe",3)     
-        # for segment in segments:
-        #     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
         self.set_event_data(
             "transcription",
@@ -90,12 +82,7 @@ class Driver(BaseDriver):
                 "transcription_duration": time.time() - start,
             },
         )
-        #self.log("finished printing segments",3)
+        
 
    
-
-def save_audio(audio):
-    audio_int16 = np.int16(audio/np.max(np.abs(audio)) * 32767)
-    write('test.wav', 16000, audio_int16)
-
 
