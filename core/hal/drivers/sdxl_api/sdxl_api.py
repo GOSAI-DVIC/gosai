fed0644f9e0acfc4f88e6e6e2f6cc0092fadc523
COLVERTYETY <niconoob1@gmail.Com>
2024-01-24 10:34:46 +0100

    clean up

--- core/hal/drivers/sdxl_api/sdxl_api.py
+++ core/hal/drivers/sdxl_api/sdxl_api.py
@@ -36,29 +36,16 @@ class Driver(BaseDriver):
                     print("redis_read is None")
                     return
                 redis_payload = redis_read
-                # print("redis_payload", redis_payload)
-                start = time.time()
                 # Send data
                 await websocket.send(redis_payload)
-                middle = time.time()
                 # Wait for the response (binary data)
                 response_bytes = await websocket.recv()
-                end = time.time()
                 self.set_event_data("response", response_bytes)
-                # print("sdxl_api", response_bytes)
-                print("sending: ", middle - start)
-                print("receiving: ", end - middle)
-                print("total: ", end - start)
         except Exception as e:
             print("sdxl_api exception", e)
             time.sleep(1)
-            # pass
     
     def loop(self):
         """Runs in a loop for the driver"""
         super().loop()
         asyncio.get_event_loop().run_until_complete(self.handle_images())
-        # time.sleep(1 / self.fps)  # Runs faster to be sure to get the current frame
-        # print("sdxl_api loop")
-        # self.set_event_data("response", "sdxl_api response")
-        # print("sdxl_api response")
